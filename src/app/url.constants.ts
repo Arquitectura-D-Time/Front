1a8c6ab9cc2aa1a06d617d8ed978dd099e59a14a
RafaelCruz15 <32246977+RafaelCruz15@users.noreply.github.com>
2019-12-20 00:11:50 -0500

    nueva ip

--- src/app/url.constants.ts
+++ src/app/url.constants.ts
@@ -1,5 +1,5 @@
 //Dennis
-export const URL = "http://35.194.102.93:5000/graphql?"
+export const URL = "http://35.200.39.92:5000/graphql?"
 //Mia
 export const URL2 = "http://35.202.129.233:5000/graphql?" 
 //Jonatan sin graphql
